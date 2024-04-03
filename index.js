892d8a51cfd61a3d8d2233cc8ae8cbbd37235810
Lee Nelson <lee@2fold.io>
2024-04-03 14:17:07 -0700

    starting point update

--- index.js
+++ index.js
@@ -1,18 +1,7 @@
-import * as dotenv from "dotenv";
-import puppeteer from "puppeteer";
-import os from "os";
-import { puppeteerConfig } from "./constants.js";
-import loginAndOpenPlugin from "./flows/loginAndOpenPlugin.js";
+import * as dotenv from 'dotenv';
 
 dotenv.config();
 
-// const runPluginAction = async (action) => {};
-
 (async () => {
-  // users os is used to determine which keyboard shortcut to use.
-  const userOs = os.platform();
-  const browser = await puppeteer.launch(puppeteerConfig);
-  const page = await browser.newPage();
-  await page.goto(process.env.FIGMA_SSO_FILE);
-  await loginAndOpenPlugin(page, userOs);
+  // Your code here
 })();
